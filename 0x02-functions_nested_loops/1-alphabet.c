9429e801f87735118861637e9512a637e77d73e2
Azibhons <nathanazibabhonomeni@gmail.com>
2022-09-13 13:51:22 -0700

    My second commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print_alphabet - prints the english alphabet from a-z.
+ * Return: Nothing.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
