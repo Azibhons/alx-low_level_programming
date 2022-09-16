8e67738acf4514b3a6e032404198c04922a9e12f
Azibhons <nathanazibabhonomeni@gmail.com>
2022-09-16 06:18:38 -0700

    My first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,14 @@
  */
 int main(void)
 {
-	char a[] = "_putchar";
-	int b;
-
-	for (b = 0; a < 9; b++)
-	{
-		_putchar(a[b]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
