296783d5fe74cfb1f26b8e0c1ef4c584f384780e
Azibhons <nathanazibabhonomeni@gmail.com>
2022-09-17 07:30:54 -0700

    My 7th commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _abs - a function that computes the absolute value of a integer
- * @i the interger to be converted.
+ * @i: the interger to be converted.
  * Return: absolute value of the input number.
  */
 int _abs(int i)
