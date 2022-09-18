06e6826da370f0fd4739d47329a3d47b20c0b218
Azibhons <nathanazibabhonomeni@gmail.com>
2022-09-18 01:47:11 -0700

    My last commit

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,45 +1,38 @@
 #include <stdio.h>
 /**
- * main - print the first 98 fibonacci numbers.
+ * main - print the first 98 fibonacci numbers
+ * starting with 1 and 2
  * Return: Nothing.
  */
 int main(void)
 {
-	int count;
-	unsigned long i, j, k;
-	unsigned long m, n, p, carry;
+	unsigned long int i, j, k, m, n, p, azi;
 
-	count = 0;
-	j = 0;
 	j = 1;
-	for (count = 1; count <= 91; count++)
+	k = 2;
+
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	{
-		k = i + j;
-		i = j;
-		j = k;
-		printf("%lu, ", k);
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	}
-	m = i % 1000;
-	i = i / 1000;
-	n = j % 1000;
-	j = j / 1000;
-	while (count <= 98)
+	m = j / 1000000000;
+	n = j % 1000000000;
+	p = k / 1000000000;
+	azi = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
 	{
-		carry = (m + n) / 1000;
-		p = (m + n) - carry * 1000;
-		k = (i + j) + carry;
-		m = n;
-		n = p;
-		i = j;
-		j = k;
-		if (p >= 100)
-			printf("%lu%lu", k, p);
-		else
-			printf("%lu0%lu", k, p);
-		if (count != 98)
-			printf(", ");
-		count++;
+		printf(", %lu", p + (azi / 1000000000));
+		printf("%lu", azi % 1000000000);
+		p = p + m;
+		m = p - m;
+		azi = azi + n;
+		n = azi - n;
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
