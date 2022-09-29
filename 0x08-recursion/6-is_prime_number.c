80784ad09df3ba8a40246b284530d71dd77a64ce
Azibhons <nathanazibabhonomeni@gmail.com>
2022-09-29 08:17:10 -0700

    My 6-is_prime_number.c file

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,12 +2,12 @@
 
 /**
  * is_prime_number - returns 1 if the input integer is a prime number
- * getNumPrim - function that take if num is 0 or 1
  * otherwise return 0
  * @n: input number
  *
  * Return: int
  */
+
 int is_prime_number(int n)
 {
 	if (n <= 1)
